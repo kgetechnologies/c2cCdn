cbd8c5e3a03e9d47fff3843b70e38b1620d70be0
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2021-05-30 15:44:06 +0530

    base Commit

---
+++ assets/js/Custom.js
@@ -0,0 +1,9 @@
+﻿var startDate = new Date();
+$('.from').datepicker({
+    autoclose: true,
+    minViewMode: 1,
+    format: 'mm/yyyy'
+}).on('changeDate', function (selected) {
+    startDate = new Date(selected.date.valueOf());
+    startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
+}); 
